15f25d983551d3a2decb1a0e38a6e517a1a5d01a
OaCantona <duringjulian@gmail.com>
2020-11-08 23:16:06 +0100

    Include athlete id in initial pin

--- init_pin.R
+++ init_pin.R
@@ -10,18 +10,18 @@ df_act_stream_raw <- df_act %>%
 
 df_act_stream <- df_act_stream_raw %>%
   filter(map_lgl(stream, ~ is.null(.x[["error"]]))) %>%
-  transmute(id, stream, act_date = as_date(start_date)) %>%
+  transmute(id, stream, `athlete.id`, act_date = as_date(start_date)) %>%
   mutate(stream = map(stream, "result")) %>%
   tidy_streams()
 
 df_act_meas <- df_act_stream %>%
-  select(id, act_date, stream) %>%
+  select(id, act_date, `athlete.id`, stream) %>%
   unnest(stream) %>%
   select(-c(original_size, resolution, series_type)) %>%
   unnest(where(purrr::is_list))
 
 df_act_meas_nested <- df_act_meas %>%
-  nest(meas = -c(id, act_date))
+  nest(meas = -c(id, act_date, `athlete.id`))
 
 df_act_meas_nested
 
@@ -29,8 +29,10 @@ board_register_github(repo = "duju211/strava_act")
 
 pin(df_act, "df_act", board = "github")
 
-walk2(
-  df_act_meas_nested$meas, df_act_meas_nested$id,
-  ~ pin(.x, str_glue("act_{.y}"), board = "github"))
+pwalk(
+  list(
+    m = df_act_meas_nested$meas, id = df_act_meas_nested$id,
+    a_id = df_act_meas_nested$`athlete.id`),
+  function(m, id, a_id) pin(m, str_glue("act_{id}_{a_id}"), board = "github"))
 
 board_disconnect("github")
